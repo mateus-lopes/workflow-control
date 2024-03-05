068fe1f35c5f6b987aa4942a5f1b073846174c6c
mateus-lopes <mateusalbano22@gmail.com>
2024-03-05 20:54:33 -0300

    FEAT: adding tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
 </template>
 
 <script lang="ts">
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,vue}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
